22034a440f6a31e7a12c71d87dc7f97bba7020e7
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-12-21 18:24:31 +0300

    JS: fix translation of delegated constructor call from secondary constructor when argument is a complex expression which translates to multiple statements. See KT-15357

--- compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
@@ -3245,6 +3245,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
             doTest(fileName);
         }
 
+        @TestMetadata("superConstructorCallWithComplexArg.kt")
+        public void testSuperConstructorCallWithComplexArg() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/classes/superConstructorCallWithComplexArg.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("typedDelegation.kt")
         public void testTypedDelegation() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/classes/typedDelegation.kt");
@@ -15428,6 +15434,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
         @TestMetadata("defaultParametersNotDuplicated.kt")
         public void testDefaultParametersNotDuplicated() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/secondaryConstructors/defaultParametersNotDuplicated.kt");
+            doTest(fileName);
+        }
+
+        @TestMetadata("delegateWithComplexExpression.kt")
+        public void testDelegateWithComplexExpression() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/secondaryConstructors/delegateWithComplexExpression.kt");
             doTest(fileName);
         }
 
